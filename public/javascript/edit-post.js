f38c6e921f3eedac645db5b5a090a59bc087452f
Charles-Nyabeze <charlesnnyabeze@gmail.com>
2022-03-12 14:41:48 -0500

    style

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,23 +1,25 @@
-const editFormHandler = async function(event) {
+async function editFormHandler(event) {
   event.preventDefault();
 
-  
-  const titleEl = document.getElementById('post-title');
-  const bodyEl = document.getElementById('post-body');
-  const postId = document.getElementById('post-id')
+  const title = document.querySelector('input[name="post-title"]').value.trim();
+  const id = window.location.toString().split('/')[
+    window.location.toString().split('/').length - 1
+  ];
+  const response = await fetch(`/api/posts/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      title
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
 
-  fetch("/api/post/" + postId.value, {
-      method: "put", 
-      body: JSON.stringify({
-          title: titleEl.value,
-          body: bodyEl.value
-      }),
-      headers: { "Content-Type": "application/json"}
-  })
-      .then(function() {
-          document.location.replace("/dashboard");
-      })
-      .catch(err => console.log(err))
+  if (response.ok) {
+    document.location.replace('/dashboard/');
+  } else {
+    alert(response.statusText);
+  }
 }
 
-document.querySelector("#edit-post-form").addEventListener("submit", editFormHandler);
+document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
